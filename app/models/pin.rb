c9955f372b21c77ba08b7f9d8a96aab8d790b9ba
Scott Rizzo <jkarizzo345@yahoo.com>
2015-11-24 00:18:16 -0800

    Increased size of picture when clicked

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -11,7 +11,7 @@ end
 
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { medium: "300x300>", thumb: "100x100>" }
+	has_attached_file :image, :styles => { medium: "300x300>", thumb: "300x300>" }
 	validates_attachment_content_type :image, :content_type=>["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	validates :image, presence: true
